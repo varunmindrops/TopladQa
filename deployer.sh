841aa3279d8db2cbc8f2252907d9a3fb82293aae
Varun Dhiman <vaurn@mindrops.com>
2022-08-12 15:51:46 +0530

    test commit

---
+++ deployer.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+echo "Deploying application ..."
+
+# Enter maintenance mode
+(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
+    # Update codebase
+    git fetch origin main
+
+# Exit maintenance mode
+php artisan up
+
+echo "Application deployed!"
