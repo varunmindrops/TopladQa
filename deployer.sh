a69ef72466e427a6d88ea100d8c726ea9e8a5ade
Varun Dhiman <vaurn@mindrops.com>
2022-08-12 17:39:13 +0530

    test final

--- deployer.sh
+++ deployer.sh
@@ -6,7 +6,7 @@ echo "Deploying application ..."
 # Enter maintenance mode
 (php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
     # Update codebase
-    git fetch origin main
+    git pull origin main
 
 # Exit maintenance mode
 php artisan up
